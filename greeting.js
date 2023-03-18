edb5cf645882ca1059dae753f27ce7ac684e4f69
Miutza94 <83984783+Miutza94@users.noreply.github.com>
2023-03-18 14:38:33 +0100

    first commit

---
+++ greeting.js
@@ -0,0 +1,13 @@
+//function getGreeting(name) {
+   // return "Hello, ${name}!";
+//}
+
+//module.exports = {
+ //   getGreeting
+//}
+
+export function getGreeting(name) {
+    return "Hello, ${name}!"; 
+}
+
+ 
